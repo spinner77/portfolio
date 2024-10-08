cb4d255b13955eec5dcea3e9524061b8ca739762
spinner77 <adele.murray1977@gmail.com>
2024-10-08 12:13:17 +0100

    Update tarot.py
    wrote give_reading function which asks for users permission for the program to give a reading

--- tarot.py
+++ tarot.py
@@ -1,3 +1,4 @@
+# Welcome ascii art and message
 print("""
  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
 | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
@@ -12,4 +13,26 @@ print("""
  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
 """)
 
-print("Welcome Friend.")
+print("Welcome Friend.\n")
+print("I can give you a Tarot reading based on your Past, Present and Future.\n")
+# Ask users permission to give a reading
+def give_reading():
+    reading_prompt = input("Would you like a reading today? y/n  ")
+    if reading_prompt == "y":
+        print("Wonderful!  Let's see what the cards can tell you today")
+        # Call past_reading function
+    elif reading_prompt == "n":
+        print("No problem.  Come back anytime to get your reading.") 
+    else:
+        print("Please type either 'y' or 'n'.")
+        return give_reading()
+
+print(give_reading())    
+
+# Make past_reading function
+
+# Make present_reading function
+
+# Make future_reading function
+
+# Give farewell message
